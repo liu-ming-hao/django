788a62b1690b9bb3c603eb6d8e648525eb057721
liu-ming-hao <haoshuo_best@foxmail.com>
2025-04-08 10:25:49 +0800

    集成sentry

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,6 +1,14 @@
 from django.urls import re_path, path
 from jobs import views
 
+
+from django.urls import path
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
+
 urlpatterns = [
     # 首页自动跳转到 职位列表
     path("", views.joblist, name="name"),
@@ -11,4 +19,5 @@ urlpatterns = [
     #提交简历
     path('resume/add/', views.ResumeCreateView.as_view(), name='resume-add'),
     path('resume/<int:pk>/', views.ResumeDetailView.as_view(), name='resume-detail'),
+    path('sentry-debug/', trigger_error),
 ]
--- settings/local.py
+++ settings/local.py
@@ -9,3 +9,20 @@ ALLOWED_HOSTS = []
 
 
 DINGTALK_WEB_HOOK = 'https://oapi.dingtalk.com/robot/send?access_token=2e55053d802ae0cc023b2c3080d3ee0463127c60d893fcec87a3f44cf40cfca0'
+
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="http://4ba3eabd5b92345f60b6d2f14f49b21c@localhost:9000/2",
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    send_default_pii=True,
+
+    # performance tracing sample rate, 采样率, 生产环境访问量过大时，建议调小（不用每一个URL请求都记录性能）
+    traces_sample_rate=1.0,  #
+
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
